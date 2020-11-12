7c47134eaaa3f6210f3941d4c3a8af312d3cdd77
Zouhair96 <54177116+Zouhair96@users.noreply.github.com>
2020-11-12 14:47:03 +0000

    Update App.java

--- src/main/java/fr/imiddleware/projets/App.java
+++ src/main/java/fr/imiddleware/projets/App.java
@@ -6,6 +6,6 @@ public class App
 {
     public static void main (String args []) 
     {
-       System.out.println(" my message : "+System.getProperty("message"));
+       System.out.println(" my message : " + System.getProperty("message"));
     }
 }
