68d4f6188114454a0e2b745491cd3a416c96495d
Zouhair96 <zouhair.benali96@gmail.com>
2020-11-12 13:56:24 +0000

    initialisation

---
+++ src/main/java/fr/imiddleware/projets/App.java
@@ -0,0 +1,12 @@
+package fr.imiddleware.projets;
+
+
+
+public class App 
+{
+    public static void main (String args []) 
+    {
+       System.out.println(" my message : "+System.getProperty("MassgeInput"));
+    	System.out.println(" my message : ");
+    }
+}
