c3a18536e3c07d7b293091e8ed614f9f73f38d6b
Zouhair96 <54177116+Zouhair96@users.noreply.github.com>
2020-11-12 14:54:06 +0000

    Update App.java

--- src/main/java/fr/imiddleware/projets/App.java
+++ src/main/java/fr/imiddleware/projets/App.java
@@ -5,7 +5,7 @@ package fr.imiddleware.projets;
 public class App 
 {
     public static void main (String args []) 
-        System sys = new System();
+        Properties  sys = new System();
     {
        System.out.println(" my message : " + sys.getProperty("message"));
     }
